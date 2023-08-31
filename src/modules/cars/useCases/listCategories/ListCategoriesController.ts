2459084600a969897e2cc4f4895759ef94ddfcf6
João Gabriel <joaogabrielbochlivio@gmail.com>
2023-08-31 06:46:09 -0300

    refactor: add list categories controller

---
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
+
+class ListCategoriesController {
+  private listCategoriesUseCase: ListCategoriesUseCase;
+
+  constructor(listCategoriesUseCase: ListCategoriesUseCase) {
+    this.listCategoriesUseCase = listCategoriesUseCase;
+  }
+
+  handle(request: Request, response: Response): Response {
+    const categories = this.listCategoriesUseCase.execute();
+
+    return response.status(200).json(categories);
+  }
+}
+
+export { ListCategoriesController };
---
+++ src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
@@ -0,0 +1,18 @@
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+import { Category } from "../../model/Category";
+
+class ListCategoriesUseCase {
+  private categoriesRepository: ICategoriesRepository;
+
+  constructor(categoriesRepository: ICategoriesRepository) {
+    this.categoriesRepository = categoriesRepository;
+  }
+
+  execute(): Category[] {
+    const categories = this.categoriesRepository.list();
+
+    return categories;
+  }
+}
+
+export { ListCategoriesUseCase };
