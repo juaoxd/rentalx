b08f59c3cf46fad6f23eda109c66629efa572919
João Gabriel <joaogabrielbochlivio@gmail.com>
2023-08-31 04:08:19 -0300

    feat: start to implement specification entity

---
+++ src/modules/cars/repositories/SpecificationsRepository.ts
@@ -0,0 +1,26 @@
+import { Specification } from "../model/Specification";
+import {
+  ICreateSpecificationDTO,
+  ISpecificationsRepository,
+} from "./ISpecificationsRepository";
+
+class SpecificationsRepository implements ISpecificationsRepository {
+  private specifications: Specification[];
+
+  constructor() {
+    this.specifications = [];
+  }
+
+  create({ name, description }: ICreateSpecificationDTO): void {
+    const specification = new Specification(name, description);
+
+    this.specifications.push(specification);
+  }
+
+  findByName(name: string): Specification {
+    const specification = this.specifications.find(specification => specification.name === name);
+    return specification;
+  }
+}
+
+export { SpecificationsRepository };
---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+import { SpecificationsRepository } from "../modules/cars/repositories/SpecificationsRepository";
+
+const specificationRoutes = Router();
+
+const specificationsRepository = new SpecificationsRepository();
+
+specificationRoutes.post("/", (request, response) => {
+  const { name, description } = request.body;
+
+  const createSpecificationService = new CreateSpecificationService(
+    specificationsRepository,
+  );
+
+  try {
+    createSpecificationService.execute({ name, description });
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+
+  return response.status(201).send();
+});
+
+export { specificationRoutes };
