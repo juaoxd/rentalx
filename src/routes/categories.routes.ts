902ff64c901e181362fcadee6c5ccf9a64b48b6c
João Gabriel <joaogabrielbochlivio@gmail.com>
2023-08-31 00:27:15 -0300

    feat: start to implement categories routes

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import { v4 as uuidV4 } from "uuid";
+import { Category } from "../model/Category";
+
+const categoriesRoutes = Router();
+
+const categories: Category[] = [];
+
+categoriesRoutes.post("/", (request, response) => {
+  const { name, description } = request.body;
+
+  const category = new Category(name, description, new Date());
+
+  categories.push(category);
+
+  return response.status(201).json(category);
+});
+
+export { categoriesRoutes };
