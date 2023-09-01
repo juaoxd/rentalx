c2e5402ed768af2bdf366d6789f1428e6d4eea45
João Gabriel <joaogabrielbochlivio@gmail.com>
2023-09-01 12:33:03 -0300

    feat(db): add typeorm and postgres

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,18 @@ import express from "express";
 import swaggerUi from "swagger-ui-express";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
+import "./database/data-source";
+import { AppDataSource } from "./database/data-source";
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
+AppDataSource.initialize()
+  .then(() => console.log("Data Source has been initialized"))
+  .catch((err) => console.error(`Data Source initialization error`, err));
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
