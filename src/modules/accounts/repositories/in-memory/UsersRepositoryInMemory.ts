c06307a2a3ffe178eacc6295c1a9323fac4c3319
João Gabriel <joaogabrielbochlivio@gmail.com>
2023-11-11 02:18:12 -0300

    tests: add authenticate user test

---
+++ src/modules/accounts/repositories/in-memory/UsersRepositoryInMemory.ts
@@ -0,0 +1,28 @@
+import { ICreateUsersDTO } from "../../dtos/ICreateUserDTO";
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../IUsersRepository";
+
+class UsersRepositoryInMemory implements IUsersRepository {
+  users: User[] = [];
+
+  async create({
+    driverLicense,
+    email,
+    name,
+    password,
+  }: ICreateUsersDTO): Promise<void> {
+    const user = new User(name, email, password, driverLicense);
+
+    this.users.push(user);
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    return this.users.find((user) => user.email === email);
+  }
+
+  async findById(id: string): Promise<User> {
+    return this.users.find((user) => user.id === id);
+  }
+}
+
+export { UsersRepositoryInMemory };
--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -1,3 +1,4 @@
+import "reflect-metadata";
 import { inject, injectable } from "tsyringe";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 import { compare } from "bcrypt";
