4217e6b94f419c3c082d631318ce13d277ec7881
João Gabriel <joaogabrielbochlivio@gmail.com>
2023-11-26 23:28:10 -0300

    fix(tests): add reflect metadata

--- src/modules/cars/useCases/listAvailableCars/ListAvailableCarsUseCase.spec.ts
+++ src/modules/cars/useCases/listAvailableCars/ListAvailableCarsUseCase.spec.ts
@@ -1,3 +1,4 @@
+import "reflect-metadata";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { ListAvailableCarsUseCase } from "./ListAvailableCarsUseCase";
 import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
@@ -8,7 +9,9 @@ let carsRepositoryInMemory: ICarsRepository;
 describe("List Cars", () => {
   beforeEach(() => {
     carsRepositoryInMemory = new CarsRepositoryInMemory();
-    listAvailableCarsUseCase = new ListAvailableCarsUseCase(carsRepositoryInMemory);
+    listAvailableCarsUseCase = new ListAvailableCarsUseCase(
+      carsRepositoryInMemory,
+    );
   });
 
   it("should be able to list all available cars", async () => {
@@ -38,7 +41,9 @@ describe("List Cars", () => {
       category_id: "Test Id",
     });
 
-    const cars = await listAvailableCarsUseCase.execute({ brand: "Car_brand_test" });
+    const cars = await listAvailableCarsUseCase.execute({
+      brand: "Car_brand_test",
+    });
 
     expect(cars).toEqual([car]);
   });
@@ -54,7 +59,9 @@ describe("List Cars", () => {
       category_id: "Test Id",
     });
 
-    const cars = await listAvailableCarsUseCase.execute({ name: "Test Car Name" });
+    const cars = await listAvailableCarsUseCase.execute({
+      name: "Test Car Name",
+    });
 
     expect(cars).toEqual([car]);
   });
@@ -70,7 +77,9 @@ describe("List Cars", () => {
       category_id: "12345",
     });
 
-    const cars = await listAvailableCarsUseCase.execute({ category_id: "12345" });
+    const cars = await listAvailableCarsUseCase.execute({
+      category_id: "12345",
+    });
 
     expect(cars).toEqual([car]);
   });
